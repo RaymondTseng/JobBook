2d64bb874901fcbfe826a6eb749c555d6cbca360
RaymondTseng <761409033@qq.com>
2016-06-05 09:54:48 +0800

    更新了几个界面及其他

--- app/src/main/java/com/example/jobbook/fragment/ArticleFragment.java
+++ app/src/main/java/com/example/jobbook/fragment/ArticleFragment.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.ColorDrawable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -13,13 +12,13 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.PopupWindow;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -38,9 +37,14 @@ public class ArticleFragment extends Fragment implements View.OnClickListener, P
     private Animation mListViewShowAnimation;
     private Animation mBlankLayoutShowAnimation;
     private Animation mBlankLayoutHideAnimation;
+    private Animation mDropImageButtonAnimation;
     private PopupWindow mMenuPopupWindow;
     private View mMenuView;
-
+    private ImageButton mDropImageButton;
+    private RadioButton mEngagementRadioButton;
+    private RadioButton mAllRadioButton;
+    private RadioButton mPoliticRadioButton;
+    private RadioButton mLifeRadioButton;
 
 
     @Override
@@ -53,9 +57,15 @@ public class ArticleFragment extends Fragment implements View.OnClickListener, P
     private void initViews(View view){
         mArticleTitleLayout = (LinearLayout) view.findViewById(R.id.article_title_ll);
         mMenuView = getActivity().getLayoutInflater().inflate(R.layout.article_title_bar_rg, null);
+        mEngagementRadioButton = (RadioButton) mMenuView.findViewById(R.id.article_title_engagement_rb);
+        mAllRadioButton = (RadioButton) mMenuView.findViewById(R.id.article_title_all_rb);
+        mPoliticRadioButton = (RadioButton) mMenuView.findViewById(R.id.article_title_politic_rb);
+        mLifeRadioButton = (RadioButton) mMenuView.findViewById(R.id.article_title_life_rb);
         mBlankLayout = (LinearLayout) view.findViewById(R.id.article_blank_ll);
         mListView = (ListView) view.findViewById(R.id.article_lv);
-        mMenuPopupWindow = new PopupWindow(mMenuView, ViewGroup.LayoutParams.MATCH_PARENT, (getmHeight()/556)*192, true);
+        mDropImageButton = (ImageButton) view.findViewById(R.id.article_title_drop_ib);
+        mMenuPopupWindow = new PopupWindow(mMenuView, ViewGroup.LayoutParams.MATCH_PARENT,
+                (getmHeight()/556)*192, true);
         mMenuPopupWindow.setBackgroundDrawable(new BitmapDrawable(getResources(),
                 Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888)));
         mMenuPopupWindow.setOutsideTouchable(true);
@@ -67,6 +77,8 @@ public class ArticleFragment extends Fragment implements View.OnClickListener, P
         mBlankLayoutHideAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.article_menu_hide);
         mListViewShowAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.article_listview_show);
         mListViewHideAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.article_listview_hide);
+        mDropImageButtonAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.article_drop_anim);
+        mDropImageButtonAnimation.setFillAfter(true);
 
     }
 
@@ -78,19 +90,31 @@ public class ArticleFragment extends Fragment implements View.OnClickListener, P
                 if(mMenuPopupWindow.isShowing() && visible == View.VISIBLE){
                     mMenuPopupWindow.dismiss();
                 }else{
+                    mDropImageButton.startAnimation(mDropImageButtonAnimation);
                     mMenuPopupWindow.showAsDropDown(v ,0 , 20);
                     mBlankLayout.startAnimation(mBlankLayoutShowAnimation);
                     mBlankLayout.setVisibility(View.VISIBLE);
                     mListView.startAnimation(mListViewShowAnimation);
+                    mDropImageButton.setImageResource(R.mipmap.down_white);
                 }
+                break;
+            case R.id.article_title_all_rb:
+                break;
+            case R.id.article_title_engagement_rb:
+                break;
+            case R.id.article_title_politic_rb:
+                break;
+            case R.id.article_title_life_rb:
                 break;
         }
     }
 
     @Override
     public void onDismiss() {
+        mDropImageButton.startAnimation(mDropImageButtonAnimation);
         mBlankLayout.startAnimation(mBlankLayoutHideAnimation);
         mListView.startAnimation(mListViewHideAnimation);
+        mDropImageButton.setImageResource(R.mipmap.up_white);
         mBlankLayout.setVisibility(View.GONE);
     }
     private int getmHeight(){
--- app/src/main/java/com/example/jobbook/fragment/JobFragment.java
+++ app/src/main/java/com/example/jobbook/fragment/JobFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.example.jobbook.R;
@@ -13,8 +15,9 @@ import com.example.jobbook.adapter.JobListViewAdapter;
 /**
  * Created by 椰树 on 2016/5/20.
  */
-public class JobFragment extends Fragment {
+public class JobFragment extends Fragment implements View.OnFocusChangeListener{
     private ListView mListView;
+    private EditText mEditText;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
@@ -24,6 +27,19 @@ public class JobFragment extends Fragment {
     }
     private void initViews(View view){
         mListView = (ListView) view.findViewById(R.id.job_lv);
+        mEditText = (EditText) view.findViewById(R.id.job_et);
         mListView.setAdapter(new JobListViewAdapter(getActivity()));
+        mEditText.setOnFocusChangeListener(this);
+        mListView.setOnFocusChangeListener(this);
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        switch(v.getId()){
+            case R.id.job_et:
+
+                break;
+
+        }
     }
 }
