be7e62f30859420e70639a083eb24845d47e064d
RaymondTseng <761409033@qq.com>
2016-07-20 13:18:51 +0800

    补充

---
+++ app/src/main/java/com/example/jobbook/job/widget/JobDetailActivity.java
@@ -0,0 +1,17 @@
+package com.example.jobbook.job.widget;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.jobbook.R;
+
+/**
+ * Created by 椰树 on 2016/7/13.
+ */
+public class JobDetailActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_detail);
+    }
+}
