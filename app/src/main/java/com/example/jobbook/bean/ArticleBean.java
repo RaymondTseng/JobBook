41a9b3d9ede6e09e3865dd29d6e10c3353c34dc1
Xu <280745397@qq.com>
2016-07-24 12:10:15 +0800

    增加四个模型类设计

--- app/src/main/java/com/example/jobbook/bean/ArticleBean.java
+++ app/src/main/java/com/example/jobbook/bean/ArticleBean.java
@@ -1,8 +1,91 @@
 package com.example.jobbook.bean;
 
+import java.io.Serializable;
+import java.util.List;
+
 /**
  * Created by Xu on 2016/7/5.
  * 文章模型类
  */
-public class ArticleBean {
+public class ArticleBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 文章标题
+     */
+    private String title;
+
+    /**
+     * 文章内容
+     */
+    private String content;
+
+    /**
+     * 文章作者
+     */
+    private PersonBean author;
+
+    /**
+     * 文章发表日期
+     */
+    private String date;
+
+    /**
+     * 文章阅读量
+     */
+    private int readingquantity;
+
+    /**
+     * 文章的评论
+     */
+    private List<ArticleCommentBean> comments;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public PersonBean getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(PersonBean author) {
+        this.author = author;
+    }
+
+    public int getReadingquantity() {
+        return readingquantity;
+    }
+
+    public void setReadingquantity(int readingquantity) {
+        this.readingquantity = readingquantity;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public List<ArticleCommentBean> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<ArticleCommentBean> comments) {
+        this.comments = comments;
+    }
 }
--- app/src/main/java/com/example/jobbook/bean/CompanyBean.java
+++ app/src/main/java/com/example/jobbook/bean/CompanyBean.java
@@ -1,7 +1,106 @@
 package com.example.jobbook.bean;
 
+import android.widget.ImageView;
+
+import java.io.Serializable;
+import java.util.List;
+
 /**
  * Created by Xu on 2016/7/15.
+ * 公司详情模型类
  */
-public class CompanyBean {
+public class CompanyBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 公司名称
+     */
+    private String name;
+
+    /**
+     * 公司图标
+     */
+    private ImageView logo;
+
+    /**
+     * 公司地址
+     */
+    private String address;
+
+    /**
+     * 公司规模
+     */
+    private String scale;
+
+    /**
+     * 公司福利
+     */
+    private String welfare;
+
+    /**
+     * 公司介绍
+     */
+    private String introduction;
+
+    /**
+     * 公司评论
+     */
+    private List<CompanyCommentBean> comments;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ImageView getLogo() {
+        return logo;
+    }
+
+    public void setLogo(ImageView logo) {
+        this.logo = logo;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getWelfare() {
+        return welfare;
+    }
+
+    public void setWelfare(String welfare) {
+        this.welfare = welfare;
+    }
+
+    public String getScale() {
+        return scale;
+    }
+
+    public void setScale(String scale) {
+        this.scale = scale;
+    }
+
+    public List<CompanyCommentBean> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CompanyCommentBean> comments) {
+        this.comments = comments;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
 }
