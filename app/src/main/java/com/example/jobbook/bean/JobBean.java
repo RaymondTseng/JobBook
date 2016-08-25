8c1ae931c139b892dcd285150644e3824253623c
RaymondTseng <761409033@qq.com>
2016-08-25 21:48:54 +0800

    完成文字简历界面

--- app/src/main/java/com/example/jobbook/bean/JobBean.java
+++ app/src/main/java/com/example/jobbook/bean/JobBean.java
@@ -5,5 +5,33 @@ package com.example.jobbook.bean;
  * 职位模型类
  */
 public class JobBean {
-
+    private static final long serialVersionUID = 1L;
+    /**
+     * id
+     */
+    private int id;
+    /**
+     * 职位名称
+     */
+    private String name;
+    /**
+     * 任职地点
+     */
+    private String location;
+    /**
+     * 薪资范围
+     */
+    private String salary;
+    /**
+     * 公司名称
+     */
+    private String company_name;
+    /**
+     * 岗位职责
+     */
+    private String responsibilities;
+    /**
+     * 任职要求
+     */
+    private String requirements;
 }
--- app/src/main/java/com/example/jobbook/bean/PersonBean.java
+++ app/src/main/java/com/example/jobbook/bean/PersonBean.java
@@ -6,7 +6,35 @@ import java.util.List;
  * Created by Xu on 2016/7/6.
  */
 public class PersonBean {
-
+    private static final long serialVersionUID = 1L;
+    /**
+     * 用户账号
+     */
+    private String account;
+    /**
+     * 用户密码
+     */
+    private String password;
+    /**
+     * 手机号码
+     */
+    private String telephone;
+    /**
+     * 用户姓名
+     */
+    private String name;
+    /**
+     * 视频简历
+     */
+    private List<VideoCV> videoCVs;
+    /**
+     * 文字简历
+     */
+    private List<TextCV> textCVs;
+    /**
+     * 收藏
+     */
+    private List<JobBean> favourite;
 
 
 }
