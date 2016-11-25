13b488e7b7c777ee803c955c1bb383df4fef312c
RaymondTseng <761409033@qq.com>
2016-11-25 19:32:49 +0800

    Moment详情

--- app/src/main/java/com/example/jobbook/userdetail/widget/UserDetailActivity.java
+++ app/src/main/java/com/example/jobbook/userdetail/widget/UserDetailActivity.java
@@ -1,9 +1,22 @@
 package com.example.jobbook.userdetail.widget;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.example.jobbook.R;
+
 /**
  * Created by root on 16-11-21.
  */
 
-public class UserDetailActivity {
+public class UserDetailActivity extends Activity{
+    private ImageView mCursor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_detail);
+    }
 
 }
