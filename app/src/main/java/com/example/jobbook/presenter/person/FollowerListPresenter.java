f972e1d40b41b384b29a72c09f90c8178b4106db
XuDeveloper <280745397@qq.com>
2018-01-27 20:14:50 +0800

    [UPDATE]更新Presenter

---
+++ app/src/main/java/com/example/jobbook/presenter/person/FollowerListPresenter.java
@@ -0,0 +1,55 @@
+package com.example.jobbook.presenter.person;
+
+import com.example.jobbook.app.MyApplication;
+import com.example.jobbook.base.BaseSubscriber;
+import com.example.jobbook.base.IBaseView;
+import com.example.jobbook.base.RxPresenter;
+import com.example.jobbook.base.contract.person.FollowerListContract;
+import com.example.jobbook.model.bean.TypePersonBean;
+import com.example.jobbook.model.http.RetrofitService;
+
+import java.util.List;
+
+/**
+ * Created by zhaoxuzhang on 2018/1/27.
+ */
+
+public class FollowerListPresenter extends RxPresenter<FollowerListContract.View> implements FollowerListContract.Presenter {
+
+    public FollowerListPresenter(FollowerListContract.View view) {
+        attach(view);
+    }
+
+    @Override
+    public void loadFollwers(String account) {
+        String myAccount = MyApplication.getAccount() == null ? "" : MyApplication.getAccount();
+        RetrofitService.loadFollowerList(account, myAccount)
+                .subscribe(new BaseSubscriber<List<TypePersonBean>>() {
+                    @Override
+                    public IBaseView getBaseView() {
+                        return mView;
+                    }
+
+                    @Override
+                    public void onNext(List<TypePersonBean> typePersonBeans) {
+                        mView.loadFanList(typePersonBeans);
+                    }
+                });
+    }
+
+    @Override
+    public void follow(String myAccount, String hisAccount) {
+        RetrofitService.follow(myAccount, hisAccount)
+                .subscribe(new BaseSubscriber<String>() {
+                    @Override
+                    public IBaseView getBaseView() {
+                        return mView;
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        mView.followSuccess();
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/example/jobbook/presenter/person/LoginPresenter.java
@@ -0,0 +1,56 @@
+package com.example.jobbook.presenter.person;
+
+import android.text.TextUtils;
+
+import com.example.jobbook.app.MyApplication;
+import com.example.jobbook.base.BaseSubscriber;
+import com.example.jobbook.base.IBaseView;
+import com.example.jobbook.base.RxPresenter;
+import com.example.jobbook.base.contract.person.LoginContract;
+import com.example.jobbook.model.bean.PersonBean;
+import com.example.jobbook.model.bean.PersonWithDeviceTokenBean;
+import com.example.jobbook.model.http.RetrofitService;
+import com.example.jobbook.util.Util;
+
+/**
+ * Created by zhaoxuzhang on 2018/1/27.
+ */
+
+public class LoginPresenter extends RxPresenter<LoginContract.View> implements LoginContract.Presenter {
+
+    public LoginPresenter(LoginContract.View view) {
+        attach(view);
+    }
+
+    @Override
+    public void loginCheck(String account, String password) {
+        if (TextUtils.isEmpty(account)) {
+            mView.setAccountError();
+            return;
+        } else if (TextUtils.isEmpty(password)) {
+            mView.setPasswordError();
+            return;
+        }
+        PersonWithDeviceTokenBean personBean = new PersonWithDeviceTokenBean();
+        personBean.setAccount(account);
+        personBean.setPassword(Util.getMD5(password));
+        personBean.setDevicetoken(MyApplication.mDevicetoken);
+        RetrofitService.login(personBean)
+                .subscribe(new BaseSubscriber<PersonBean>() {
+                    @Override
+                    public IBaseView getBaseView() {
+                        return mView;
+                    }
+
+                    @Override
+                    public void onNext(PersonBean personBean) {
+                        mView.switch2Person(personBean);
+                    }
+                });
+    }
+
+    @Override
+    public void destroy() {
+        mView = null;
+    }
+}
